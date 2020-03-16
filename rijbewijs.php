28ce7166f75b0fa9d752f16c8be6dfd29116a811
Kimberly Hogeveen <2103405@talnet.nl>
2020-03-16 14:59:17 +0100

    Uitwerking van de exercise

---
+++ rijbewijs.php
@@ -0,0 +1,9 @@
+<?php
+echo "Hoe oud ben je?";
+$getal1= readline();
+if ($getal1 < "16,5") {
+    echo "Helaas, je mag nog niet beginnen met rijlessen";
+}else{
+    echo "Je mag beginnen met rijlessen!";
+}
+    ?>
